d6f076ab673a6d255682c09c65cfd255196cf498
Bartosz <burtekdotryw@gmail.com>
2024-11-27 15:07:27 +0100

    [base] Footer

--- src/app/@footer/default.tsx
+++ src/app/@footer/default.tsx
@@ -1,3 +1,40 @@
+import styles from './footer.module.scss';
+
+
 export default function Footer() {
-    return null;
+    return (
+        <footer className={styles.footer}>
+            <hr />
+            <p>
+                {'Powered by '}
+                <a
+                    rel="noopener noreferrer nofollow"
+                    href="https://nextjs.org/"
+                >
+                    NextJS
+                </a>
+                {' and '}
+                <a
+                    rel="noopener noreferrer nofollow"
+                    href="https://velite.js.org/"
+                >
+                    Velite
+                </a>
+                {', hosted on '}
+                <a
+                    rel="noopener noreferrer nofollow"
+                    href="https://vercel.com"
+                >
+                    Vercel
+                </a>
+                {', source available on '}
+                <a
+                    rel="noopener noreferrer nofollow"
+                    href="https://github.com/burtek/homepage"
+                >
+                    GitHub
+                </a>
+            </p>
+        </footer>
+    );
 }
