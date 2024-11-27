3ac7a928bc799f1eb06862012420fde8e92bdc36
Bartosz <burtekdotryw@gmail.com>
2024-11-27 15:28:30 +0100

    [blog] Add github link to post changes

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -57,7 +57,11 @@ function BlogPost() {
                 <time dateTime={post.created}>{formatTime(post.created)}</time>
                 {post.created !== post.updated && (
                     <>
-                        {', zaktualizowano: '}
+                        {', '}
+                        <a href={`https://github.com/burtek/homepage/commits/master/src/content/posts/${post.id}`}>
+                            zaktualizowano
+                        </a>
+                        {': '}
                         <time dateTime={post.updated}>{formatTime(post.updated)}</time>
                     </>
                 )}
