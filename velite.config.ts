7ea7841739faa4fb6aa5f71594bc40ee791677b2
Bartosz <2940958+burtek@users.noreply.github.com>
2024-12-31 20:21:01 +0100

    Update velite.config.ts

--- velite.config.ts
+++ velite.config.ts
@@ -24,7 +24,7 @@ const timestamp = (type: 'created' | 'modified') =>
                 addIssue({ fatal: false, code: 'custom', message: '`s.timestamp()` schema will resolve the value from `git log` command' });
             }
             const { stdout } = await execAsync(command);
-            console.log(meta.path, type, stdout);
+            console.log(meta.path, type, JSON.stringify(stdout));
             const date = stdout ? new Date(stdout) : new Date();
             return date.toISOString();
         });
