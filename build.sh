0ff511890ea13970017eddba1f564ecf31893df3
Bartosz Dotryw <burtekdotryw@gmail.com>
2024-12-31 23:21:06 +0100

    test

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Clean up any existing .git folder (shallow clone)
+rm -rf .git
+
+# Reinitialize the Git repository and fetch full history
+git init
+git remote add origin https://$VERCEL_GIT_REPO_OWNER:$GITHUB_TOKEN@github.com/$VERCEL_GIT_REPO_SLUG.git
+git fetch --depth=0 --all --unshallow
+git reset --hard origin/main
+git checkout $VERCEL_GIT_COMMIT_SHA
+
+# Proceed with the build process
+yarn
+yarn build
