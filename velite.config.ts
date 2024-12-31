ddf3093e937c6585b07ea60057b4e5f5840ba448
Bartosz <2940958+burtek@users.noreply.github.com>
2024-12-31 20:03:06 +0100

    Update velite.config.ts

--- velite.config.ts
+++ velite.config.ts
@@ -20,7 +20,7 @@ const timestamp = (type: 'created' | 'modified') =>
         .custom<string | undefined>(i => i === undefined || typeof i === 'string')
         .transform<string>(async (value, { meta, addIssue }) => {
 
-            const { stdout: s } = await execAsync(`git log --format=%cd ${meta.path}`);
+            const { stdout: s } = await execAsync(`git log --oneline -5`);
             console.log(s);            
 
             const command = `git log --format=%cd ${type === 'created' ? '--reverse' : ''} ${meta.path} | head -n1`;
