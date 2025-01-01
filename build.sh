0dd577750bf05dd46c290cb599081be43468428e
Bartosz <2940958+burtek@users.noreply.github.com>
2025-01-01 08:06:20 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ rm -rf .git
 # Reinitialize the Git repository and fetch full history
 git init
 git remote add origin https://$VERCEL_GIT_REPO_OWNER:$GITHUB_TOKEN@github.com/$VERCEL_GIT_REPO_SLUG.git
-git remote
 git fetch --depth=0
 git reset --hard origin/main
 git checkout $VERCEL_GIT_COMMIT_SHA
